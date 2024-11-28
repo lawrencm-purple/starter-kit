2de29a352ff0cc12dce9d7b1ff7f080598a966d9
Matt <matthew.lawrence@purple.telstra.com>
2024-11-28 13:25:25 +0800

    adds multiple next sites

---
+++ apps/docs/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/lawrencm/Projects/starterkit/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/lawrencm/Projects/starterkit/node_modules/.pnpm/@docusaurus+theme-classic@3.6.3_@types+react@18.3.12_acorn@8.14.0_eslint@8.57.1_react-dom@18._obagmvzrxfottesniaheecu2ca/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/lawrencm/Projects/starterkit/node_modules/.pnpm/@docusaurus+theme-classic@3.6.3_@types+react@18.3.12_acorn@8.14.0_eslint@8.57.1_react-dom@18._obagmvzrxfottesniaheecu2ca/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/lawrencm/Projects/starterkit/apps/docs/src/css/custom.css"),
+];
