4d4c392d6671759606de5e927247ea061b6d1d83
Matt <matthew.lawrence@purple.telstra.com>
2024-12-05 09:53:14 +0800

    working next

---
+++ apps/documentation/.storybook/main.js
@@ -0,0 +1,32 @@
+import { dirname, join } from "path";
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../../../packages/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../../../packages/auth/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../../../turbo/generators/docs/*.@(mdx)",
+    "../../**/docs/*.@(mdx)",
+  ],
+  addons: [
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-interactions"),
+  ],
+  framework: {
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {},
+  },
+};
+export default config;
